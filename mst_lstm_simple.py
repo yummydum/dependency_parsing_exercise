924240e7bc6ad7d409b9c6c9b7ea994f2f466a75
yummydum <atsushisumita0421@gmail.com>
2019-06-28 21:07:05 +0900

    Utilize GPU / use only first 1000 data

--- mst_lstm_simple.py
+++ mst_lstm_simple.py
@@ -13,6 +13,12 @@ logger = set_logger(__name__)
 # Fix seed
 torch.manual_seed(1)
 
+# manage device
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
 class BiLSTM_Parser_simple(nn.Module):
 
     def __init__(self,
@@ -132,6 +138,7 @@ if __name__ == '__main__':
 
     # Init model
     model = BiLSTM_Parser_simple(**config_dict["model_param"])
+    model.to(device)
 
     # Train setting
     optimizer = optim.Adam(model.parameters(),config_dict["learning_rate"])
@@ -143,7 +150,9 @@ if __name__ == '__main__':
     for epoch in range(epoch_num):  # epoch = 0
         running_loss = 0
         for i,data in enumerate(train_data):  # i = 0; data = train_data[i]
-            score_matrix = model(data[0],data[1])
+            word_tensor = data[0].to(device)
+            pos_tensor  = data[1].to(device)
+            score_matrix = model(word_tensor,pos_tensor)
             loss = model.calc_loss(score_matrix,data[2])
             (loss / batch_size).backward()
             running_loss += loss.item() / batch_size
@@ -158,21 +167,50 @@ if __name__ == '__main__':
                 loss_tracker.append(running_loss)
                 running_loss = 0
 
-        # Report loss for this epoch in dev data
-        with torch.no_grad():
-            running_loss_dev = 0
-            for j,data in enumerate(dev_data):
-                score_matrix = model(data[0],data[1])
-                loss = model.calc_loss(score_matrix,data[2])
-                running_loss_dev += loss.item()
-                if i % 1000 == 999:
-                    logger.debug(f"Now validating model; {j}th dev data now")
-            mean_loss_dev = running_loss_dev/len(dev_data)
-            logger.debug(f"Current mean loss for dev data is {mean_loss_dev}")
+            # stop at the 30th batch
+            if i > batch_size*30:
+                continue
 
         # Save model
         result_path = result_dir_path / f"model_epoch{epoch}.pt"
         torch.save(model,str(result_path))
+
+
+    # # Start train
+    #  loss_tracker = []
+    # for epoch in range(epoch_num):  # epoch = 0
+    #     running_loss = 0
+    #     for i,data in enumerate(train_data):  # i = 0; data = train_data[i]
+    #         score_matrix = model(data[0],data[1])
+    #         loss = model.calc_loss(score_matrix,data[2])
+    #         (loss / batch_size).backward()
+    #         running_loss += loss.item() / batch_size
+    #         # Accumualte the gradient for each batch
+    #         if (i % batch_size == (batch_size-1)) or (i == len(train_data)-1):
+    #             # Update param
+    #             optimizer.step()
+    #             model.zero_grad()
+    #             # Record the scores
+    #             logger.debug(f"Now at {i}th data in epoch {epoch}")
+    #             logger.debug(f"Current loss is {running_loss}")
+    #             loss_tracker.append(running_loss)
+    #             running_loss = 0
+    #
+    #     # Report loss for this epoch in dev data
+    #     with torch.no_grad():
+    #         running_loss_dev = 0
+    #         for j,data in enumerate(dev_data):
+    #             score_matrix = model(data[0],data[1])
+    #             loss = model.calc_loss(score_matrix,data[2])
+    #             running_loss_dev += loss.item()
+    #             if i % 1000 == 999:
+    #                 logger.debug(f"Now validating model; {j}th dev data now")
+    #         mean_loss_dev = running_loss_dev/len(dev_data)
+    #         logger.debug(f"Current mean loss for dev data is {mean_loss_dev}")
+    #
+    #     # Save model
+    #     result_path = result_dir_path / f"model_epoch{epoch}.pt"
+    #     torch.save(model,str(result_path))
 
 # # Check forward() and loss funtion
 # data = dev_data[1]
